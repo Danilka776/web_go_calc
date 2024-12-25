7babceda0fa7ab300b7a97ae6e9110eafa8baa5e
Тарасов Данил Дамирович <s02200491@gse.cs.msu.ru>
2024-12-25 22:49:09 +0300

    package

--- app/application.go
+++ app/application.go
@@ -1,9 +1,13 @@
-package main
+package application
 
 import (
+	"bufio"
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 	"strings"
+	//calc "github.com/Danilka776/web_go_calc/calculate"
 )
 
 type RequestBody struct {
@@ -16,6 +20,51 @@ type ResponseBody struct {
 
 type ErrorResponse struct {
 	Error string `json:"error"`
+}
+
+type Config struct {
+	Addr string
+}
+
+func ConfigFromEnv() *Config {
+	config := new(Config)
+	config.Addr = os.Getenv("PORT")
+	if config.Addr == "" {
+		config.Addr = "8080"
+	}
+	return config
+}
+
+type Application struct {
+	config *Config
+}
+
+func New() *Application {
+	return &Application{
+		config: ConfigFromEnv(),
+	}
+}
+
+func (a *Application) Run() error {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Failed to read expression from console")
+		}
+		text = strings.TrimSpace(text)
+		if text == "exit" {
+			log.Println("application was successfully closed")
+			return nil
+		}
+		//вычисляем выражение
+		result, err := Calc(text)
+		if err != nil {
+			log.Println(text, " calculation failed wit error: ", err)
+		} else {
+			log.Println(text, "=", result)
+		}
+	}
 }
 
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
@@ -51,7 +100,7 @@ func main() {
 		w.WriteHeader(http.StatusInternalServerError) // 500
 		json.NewEncoder(w).Encode(ErrorResponse{Error: "Internal server error"})
 	})
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		panic("1Internal server error")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic("Internal server error")
 	}
 }
